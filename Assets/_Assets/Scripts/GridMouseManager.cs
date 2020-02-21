4b0825c472ccc959908ae88594e36fd76af8280b
richardbonneau <richardbonneau@hotmail.com>
2020-02-21 15:08:13 -0500

    cursor selector works

---
+++ Assets/_Assets/Scripts/GridMouseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMouseManager : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject mapSelector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+            mapSelector.transform.position = hit.point;
+            print(hit.point);
+        } else mapSelector.transform.position = new Vector3(999,999,999);
+    }
+}
