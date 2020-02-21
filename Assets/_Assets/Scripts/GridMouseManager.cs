77ee653d141eb3b8c7c93f68f9bb789f6283ad6a
richardbonneau <richardbonneau@hotmail.com>
2020-02-21 15:56:19 -0500

    character movement

--- Assets/_Assets/Scripts/GridMouseManager.cs
+++ Assets/_Assets/Scripts/GridMouseManager.cs
@@ -1,28 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GridMouseManager : MonoBehaviour
 {
     public Camera camera;
     public GameObject mapSelector;
+    public GameObject player;
+
+    NavMeshAgent playerAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAgent = player.GetComponent<NavMeshAgent>();
     
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
             if(Physics.Raycast(ray, out hit)) {
+            Vector3 snappedCoordinates =new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
             Transform objectHit = hit.transform;
-            mapSelector.transform.position = new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
+            mapSelector.transform.position = snappedCoordinates;
             print(hit.point);
+            playerAgent.destination=snappedCoordinates;
         } else mapSelector.transform.position = new Vector3(999,mapSelector.transform.position.y,999);
+        }
     }
 }
