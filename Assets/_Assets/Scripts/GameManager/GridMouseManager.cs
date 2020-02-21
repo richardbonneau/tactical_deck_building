a6f13929381eb884f164cdaee8ea9f85cc23126f
richardbonneau <richardbonneau@hotmail.com>
2020-02-21 16:49:37 -0500

    commit

--- Assets/_Assets/Scripts/GameManager/GridMouseManager.cs
+++ Assets/_Assets/Scripts/GameManager/GridMouseManager.cs
@@ -8,29 +8,32 @@ public class GridMouseManager : MonoBehaviour
     public Camera camera;
     public GameObject mapSelector;
     public GameObject player;
-
+    PlayerStatus playerstatus;
     NavMeshAgent playerAgent;
     // Start is called before the first frame update
     void Start()
     {
+        playerstatus = GetComponent<PlayerStatus>();
         playerAgent = player.GetComponent<NavMeshAgent>();
-    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
             RaycastHit hit;
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
             if(Physics.Raycast(ray, out hit)) {
             Vector3 snappedCoordinates =new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
             Transform objectHit = hit.transform;
             mapSelector.transform.position = snappedCoordinates;
-            print(hit.point);
-            playerAgent.destination=snappedCoordinates;
+            print(snappedCoordinates);
+            if (Input.GetMouseButtonDown(0)) {
+                playerAgent.destination=snappedCoordinates;
+                playerstatus.currentCoordinates = snappedCoordinates;
+                // I need an array of destinations that the agent will go through one by one
+            }
+            
         } else mapSelector.transform.position = new Vector3(999,mapSelector.transform.position.y,999);
-        }
+        
     }
 }
---
+++ Assets/_Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int remainingMovements = 5;
+    public Vector3 currentCoordinates;
+    // if more than one vector is changed, its a diagonal movement, therefore it costs 2
+
+}
