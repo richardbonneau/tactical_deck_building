e97356ff847537c33184fd2f67d5bcd0daf7f715
richardbonneau <richardbonneau91@gmail.com>
2020-02-22 16:35:33 -0500

    beginning of grid creation

--- Assets/_Assets/Scripts/GameManager/GridMouseManager.cs
+++ Assets/_Assets/Scripts/GameManager/GridMouseManager.cs
@@ -16,40 +16,57 @@ public class GridMouseManager : MonoBehaviour
     {
         playerstatus = GetComponent<PlayerStatus>();
         playerAgent = player.GetComponent<NavMeshAgent>();
-        Pathfinder();
+        
     }
 
-    void Pathfinder(){
+    void Pathfinder(Vector3 startPos,Vector3 endPos){
     
         // Nodes that we want to calculate the F cost of
-        List<GridNode> openNodes = new List<GridNode>();
+        List<Node> openNodes = new List<Node>();
         // Nodes that have already been evaluated
-        List<GridNode> closedNodes = new List<GridNode>();
+        List<Node> closedNodes = new List<Node>();
         bool pathFound = false;
         
-        GridNode playerNode = new GridNode(player.transform.position.x,player.transform.position.z);
-        print(playerNode.x);
-        openNodes.Add(playerNode);
+        // Node playerNode = new Node(startPos.x,startPos.z);
+        // Node targetNode = new Node(endPos.x,endPos.z);
         
+        // List<Node> neighbours = GetNeighbours(playerNode);
 
+
+
+        // openNodes.Add(playerNode);
         // while(!pathFound){
-        //     Vector3 lowestCostNode;
-        //     foreach(Vector3 node in openNodes){
-                
+        //     Node lowestCostNode = playerNode;
+        //     foreach(Node node in openNodes){
+        //         if(lowestCostNode.fCost < node.fCost) {
+        //             lowestCostNode = node;
+        //             closedNodes.Remove(node);
+        //             openNodes.Add(node);
+        //             }
         //     }
+
         // }
-
-        
         // foreach(Vector3 neighbour in neighbourList){
-
         // }
     }
-    // int GetDistance(Vector3 nodeA, Vector3 nodeB){
-    //     int changingCoordinates = 0;
-    //     if(nodeA.x == nodeB.x) changingCoordinates++;
-    //     if(nodeA.z == nodeB.z) changingCoordinates++;
-    //     if(changingCoordinates == 1) return 10;
-    //     else if(changingCoordinates == 2) return 14;
+
+	// int GetDistance(Node nodeA, Node nodeB) {
+	// 	int dstX = Mathf.Abs(nodeA.x - nodeB.x);
+	// 	int dstY = Mathf.Abs(nodeA.y - nodeB.y);
+
+	// 	if (dstX > dstY)
+	// 		return 14*dstY + 10* (dstX-dstY);
+	// 	return 14*dstX + 10 * (dstY-dstX);
+	// }
+
+    // public List<Node> GetNeighbours(Node node){
+    //     List<Node> neighbours = new List<Node>();
+
+    //     for (int x = -1; x <= 1; x++) {
+	// 	    for (int y = -1; y <= 1; y++) {
+	// 		if (x == 0 && y == 0) continue;
+    //         }
+    //     }
     // }
 
     // Update is called once per frame
@@ -61,10 +78,12 @@ public class GridMouseManager : MonoBehaviour
             Vector3 snappedCoordinates =new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
             Transform objectHit = hit.transform;
             mapSelector.transform.position = snappedCoordinates;
-            print(snappedCoordinates);
+            // print(snappedCoordinates);
             if (Input.GetMouseButtonDown(0)) {
-                playerAgent.destination=snappedCoordinates;
-                playerstatus.currentCoordinates = snappedCoordinates;
+                Pathfinder(player.transform.position, snappedCoordinates);
+
+                // playerAgent.destination=snappedCoordinates;
+                // playerstatus.currentCoordinates = snappedCoordinates;
                 // I need an array of destinations that the agent will go through one by one
             }
             
@@ -72,3 +91,29 @@ public class GridMouseManager : MonoBehaviour
         
     }
 }
+
+
+
+
+
+
+// Off combat Movements 
+
+    // void Update()
+    // {
+    //         RaycastHit hit;
+    //         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+    //         if(Physics.Raycast(ray, out hit)) {
+    //         Vector3 snappedCoordinates =new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
+    //         Transform objectHit = hit.transform;
+    //         mapSelector.transform.position = snappedCoordinates;
+    //         print(snappedCoordinates);
+    //         if (Input.GetMouseButtonDown(0)) {
+    //             playerAgent.destination=snappedCoordinates;
+    //             playerstatus.currentCoordinates = snappedCoordinates;
+    //             // I need an array of destinations that the agent will go through one by one
+    //         }
+            
+    //     } else mapSelector.transform.position = new Vector3(999,mapSelector.transform.position.y,999);
+        
+    // }
---
+++ Assets/_Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Node
+    {
+    public int x;
+    public int z;
+    public Vector3 worldPosition;
+    public bool walkable;
+
+    // Distance from Starting Node
+    public int gCost;
+    // Distance from End Node
+    public int hCost;
+    // gCost + hCost
+    public int fCost;
+    public Node parent;
+
+    // Put in place some kind of collider detection
+    
+
+    public Node(int _xCoordinates, int _zCoordinates, Vector3 _worldPosition, bool _walkable){
+        x = _xCoordinates;
+        z = _zCoordinates;
+        worldPosition = _worldPosition;
+        walkable = _walkable;
+        
+    }
+
+
+
+    
+    }
