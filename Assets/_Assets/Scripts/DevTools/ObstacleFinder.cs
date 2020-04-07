e77b8d52eb370f50d410f670524c8436fc07fbde
richardbonneau <richardbonneau91@gmail.com>
2020-04-07 12:13:07 -0400

    a

--- Assets/_Assets/Scripts/DevTools/ObstacleFinder.cs
+++ Assets/_Assets/Scripts/DevTools/ObstacleFinder.cs
@@ -14,60 +14,76 @@ public class ObstacleFinder : MonoBehaviour
     public List<Vector2> obstacleCoordinatesList = new List<Vector2>();
     bool listSaved = false;
 
-    void Awake(){
+    void Awake()
+    {
         gridScript = GameObject.FindWithTag("GameManager").transform.GetChild(0).GetComponent<GridCreator>();
-        
+
     }
-    void Start(){
+    void Start()
+    {
         grid = gridScript.grid;
-        print("grid:"+gridScript);
+        print("grid:" + gridScript);
     }
-    void FixedUpdate(){
-       if(!obstacleScanDone) {
-       if(!hitObstacleLastUpdate){
-           
-        //    print("grid.GetLength(1) "+grid.GetLength(0)+"currentObstacleFinderZ "+currentObstacleFinderZ+" currentObstacleFinderX"+currentObstacleFinderX);
-        if(currentObstacleFinderZ < grid.GetLength(0)){
-            transform.position = grid[currentObstacleFinderX,currentObstacleFinderZ].worldPosition;
-            // Instantiate(test, grid[currentObstacleFinderX,currentObstacleFinderZ].worldPosition, Quaternion.identity);
-            currentObstacleFinderZ++;
-            } else if(currentObstacleFinderX < grid.GetLength(1)-1){
-                currentObstacleFinderX++;
-                currentObstacleFinderZ=0;
-            } else obstacleScanDone = true;
-        } else {
-            print("obstacle else");
-            
-            hitObstacleLastUpdate = false;
+    void FixedUpdate()
+    {
+        if (!obstacleScanDone)
+        {
+            if (!hitObstacleLastUpdate)
+            {
+
+                //    print("grid.GetLength(1) "+grid.GetLength(0)+"currentObstacleFinderZ "+currentObstacleFinderZ+" currentObstacleFinderX"+currentObstacleFinderX);
+                if (currentObstacleFinderZ < grid.GetLength(0))
+                {
+                    transform.position = grid[currentObstacleFinderX, currentObstacleFinderZ].worldPosition;
+                    // Instantiate(test, grid[currentObstacleFinderX,currentObstacleFinderZ].worldPosition, Quaternion.identity);
+                    currentObstacleFinderZ++;
+                }
+                else if (currentObstacleFinderX < grid.GetLength(1) - 1)
+                {
+                    currentObstacleFinderX++;
+                    currentObstacleFinderZ = 0;
+                }
+                else obstacleScanDone = true;
             }
-       
-    } else if(!listSaved){
-        print("scan done else");
-        print(obstacleCoordinatesList);
-        obstacleCoordinatesList = obstacleCoordinatesList.Distinct().ToList();
-        string path = "Assets/_Assets/DevTools/ObstaclesList.txt";
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine("public List<Vector2> obstaclesCoordinates = new List<Vector2>(){");
-        foreach (Vector2 coord in obstacleCoordinatesList){
-            writer.WriteLine("new Vector2("+coord.x+"f,"+coord.y+"f)"+",");
+            else
+            {
+                print("obstacle else");
+
+                hitObstacleLastUpdate = false;
+            }
+
         }
-              writer.WriteLine("};");
-        writer.Close();
-        listSaved = true;
+        else if (!listSaved)
+        {
+            print("scan done else");
+            print(obstacleCoordinatesList);
+            obstacleCoordinatesList = obstacleCoordinatesList.Distinct().ToList();
+            string path = "Assets/_Assets/DevTools/ObstaclesList.txt";
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine("public static List<Vector2> obstaclesCoordinates = new List<Vector2>(){");
+            foreach (Vector2 coord in obstacleCoordinatesList)
+            {
+                writer.WriteLine("new Vector2(" + coord.x + "f," + coord.y + "f)" + ",");
+            }
+            writer.WriteLine("};");
+            writer.Close();
+            listSaved = true;
+        }
     }
-    }
-  
-    
+
+
     // Start is called before the first frame update
-    void OnTriggerEnter(Collider col){
-        if(col.transform.CompareTag("Obstacle")){
-            print("obstacle found: "+col.transform.name);
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Obstacle"))
+        {
+            print("obstacle found: " + col.transform.name);
             // Node node = gridScript.NodeFromWorldPoint(transform.position);
             // node.walkable = false;
-            Vector2 newCoord = new Vector2(currentObstacleFinderX,currentObstacleFinderZ);
+            Vector2 newCoord = new Vector2(currentObstacleFinderX, currentObstacleFinderZ);
             // if(obstacleCoordinatesList.Count > 0 && obstacleCoordinatesList[obstacleCoordinatesList.Count-1] != newCoord)
             obstacleCoordinatesList.Add(newCoord);
-            transform.position = new Vector3(0,10,0);
+            transform.position = new Vector3(0, 10, 0);
             hitObstacleLastUpdate = true;
         }
     }
--- Assets/_Assets/Scripts/GameManager/GridCreator.cs
+++ Assets/_Assets/Scripts/GameManager/GridCreator.cs
@@ -4,165 +4,97 @@ using UnityEngine;
 
 public class GridCreator : MonoBehaviour
 {
-    Vector3 gridSpawnPoint = new Vector3(-10,0,-10);
-    public int gridSizeX = 19;
-    public int gridSizeZ = 19;
-    Node outOfBounds = new Node(0,0,new Vector3(0,10,0),false);
+    Vector3 gridSpawnPoint = new Vector3(-10, 0, -10);
+    public int gridSizeX = 20;
+    public int gridSizeZ = 20;
+    Node outOfBounds = new Node(0, 0, new Vector3(0, 10, 0), false);
     public Node[,] grid;
     public List<Node> path;
-    public List<Vector2> obstaclesCoordinates = new List<Vector2>(){
-new Vector2(1f,17f),
-new Vector2(2f,5f),
-new Vector2(2f,6f),
-new Vector2(2f,7f),
-new Vector2(2f,10f),
-new Vector2(2f,11f),
-new Vector2(2f,12f),
-new Vector2(2f,17f),
-new Vector2(3f,17f),
-new Vector2(4f,17f),
-new Vector2(5f,17f),
-new Vector2(6f,3f),
-new Vector2(6f,4f),
-new Vector2(6f,5f),
-new Vector2(6f,6f),
-new Vector2(6f,7f),
-new Vector2(6f,8f),
-new Vector2(6f,9f),
-new Vector2(7f,3f),
-new Vector2(7f,4f),
-new Vector2(7f,5f),
-new Vector2(7f,6f),
-new Vector2(7f,7f),
-new Vector2(7f,8f),
-new Vector2(7f,9f),
-new Vector2(7f,11f),
-new Vector2(7f,12f),
-new Vector2(7f,17f),
-new Vector2(7f,18f),
-new Vector2(8f,3f),
-new Vector2(8f,4f),
-new Vector2(8f,5f),
-new Vector2(8f,6f),
-new Vector2(8f,7f),
-new Vector2(8f,8f),
-new Vector2(8f,9f),
-new Vector2(8f,10f),
-new Vector2(8f,11f),
-new Vector2(8f,12f),
-new Vector2(8f,16f),
-new Vector2(8f,17f),
-new Vector2(8f,18f),
-new Vector2(9f,10f),
-new Vector2(9f,11f),
-new Vector2(9f,16f),
-new Vector2(9f,17f),
-new Vector2(10f,14f),
-new Vector2(10f,15f),
-new Vector2(10f,16f),
-new Vector2(10f,17f),
-new Vector2(10f,18f),
-new Vector2(10f,19f),
-new Vector2(10f,20f),
-new Vector2(11f,14f),
-new Vector2(11f,15f),
-new Vector2(11f,16f),
-new Vector2(11f,17f),
-new Vector2(11f,18f),
-new Vector2(11f,19f),
-new Vector2(11f,20f),
-new Vector2(12f,14f),
-new Vector2(12f,15f),
-new Vector2(12f,16f),
-new Vector2(12f,17f),
-new Vector2(12f,18f),
-new Vector2(12f,19f),
-new Vector2(12f,20f),
-new Vector2(15f,8f),
-new Vector2(15f,9f),
-new Vector2(15f,10f),
-new Vector2(15f,11f),
-new Vector2(15f,12f),
-new Vector2(15f,13f),
-new Vector2(15f,14f),
-new Vector2(15f,16f),
-new Vector2(15f,17f),
-new Vector2(15f,18f),
-new Vector2(18f,3f),
-new Vector2(18f,4f),
-new Vector2(18f,5f)
-};
 
-
-    void Awake() {
+    public static List<Vector2> obstaclesCoordinates = new List<Vector2>();
+    void Awake()
+    {
         CreateGrid();
     }
-    void FixedUpdate(){
-        
-    
-    }
-    
 
-    void CreateGrid(){
+
+    void CreateGrid()
+    {
+        print("obstaclesCoordinates" + obstaclesCoordinates.Count);
         grid = new Node[gridSizeX, gridSizeZ];
-        for (int x= 0; x<gridSizeX; x++){
-            for(int z=0;z<gridSizeZ; z++){
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int z = 0; z < gridSizeZ; z++)
+            {
                 bool walkable = true;
-                foreach(Vector2 obstacleCoord in obstaclesCoordinates){
-                    if(obstacleCoord.x == x && obstacleCoord.y == z){
+                foreach (Vector2 obstacleCoord in obstaclesCoordinates)
+                {
+                    print("po");
+                    if (obstacleCoord.x == x && obstacleCoord.y == z)
+                    {
                         walkable = false;
                         // obstaclesCoordinates.Remove(obstacleCoord);
                     }
                 }
-                
+
                 // if(x == 10 && z == 10 || x == 11 && z == 10 || x == 12 && z ==10) walkable = false;
-                grid[x,z] = new Node(x,z,new Vector3(gridSpawnPoint.x + x,0,gridSpawnPoint.z + z),walkable);  
+                grid[x, z] = new Node(x, z, new Vector3(gridSpawnPoint.x + x, 0, gridSpawnPoint.z + z), walkable);
             }
         }
     }
-    
-    public void ResetAllNodeCosts(){
-        foreach(Node node in grid){
+
+    public void ResetAllNodeCosts()
+    {
+        foreach (Node node in grid)
+        {
             node.Reset();
         }
     }
 
-    public Node NodeFromWorldPoint(Vector3 worldPosition){
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
         int x = Mathf.RoundToInt(worldPosition.x - gridSpawnPoint.x);
         int z = Mathf.RoundToInt(worldPosition.z - gridSpawnPoint.z);
-        if(x < 0 || z < 0 || x > grid.GetLength(0) - 1 || z > grid.GetLength(1) - 1) return outOfBounds;
-        return grid[x,z];
+        if (x < 0 || z < 0 || x > grid.GetLength(0) - 1 || z > grid.GetLength(1) - 1) return outOfBounds;
+        return grid[x, z];
     }
 
-    public List<Node> GetNeighbours(Node node){
-    List<Node> neighbours = new List<Node>();
-        for (int x = -1; x <= 1; x++) {
-	        for (int z = -1; z <= 1; z++) {
-		    if (x == 0 && z == 0) continue;
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                if (x == 0 && z == 0) continue;
                 int checkX = node.x + x;
                 int checkZ = node.z + z;
-                if (checkX >= 0 && checkX < gridSizeX && checkZ >= 0 && checkZ < gridSizeZ) {
-                    neighbours.Add(grid[checkX,checkZ]);
-				}
+                if (checkX >= 0 && checkX < gridSizeX && checkZ >= 0 && checkZ < gridSizeZ)
+                {
+                    neighbours.Add(grid[checkX, checkZ]);
+                }
             }
         }
         return neighbours;
     }
-    
 
-    
-	void OnDrawGizmos() {
+
+
+    void OnDrawGizmos()
+    {
         // print("path "+path);
-		// Gizmos.DrawWireCube(transform.position,new Vector3(gridSizeX.x,1,gridSizeZ.y));
+        // Gizmos.DrawWireCube(transform.position,new Vector3(gridSizeX.x,1,gridSizeZ.y));
 
-		if (grid != null) {
-			foreach (Node n in grid) {
-				Gizmos.color = (n.walkable)?Color.white:Color.red;
-				if (path != null)
-					if (path.Contains(n))
-						Gizmos.color = Color.black;
-				Gizmos.DrawCube(n.worldPosition, Vector3.one * (1000-.1f));
-			}
-		}
-	}
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (path != null)
+                    if (path.Contains(n))
+                        Gizmos.color = Color.black;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (1000 - .1f));
+            }
+        }
+    }
 }
