74152c475a9c0c9cd23e68c4fe7e9634a3f18cd0
richardbonneau <richardbonneau91@gmail.com>
2020-04-07 14:04:41 -0400

    pathfind on edges fixed

--- Assets/_Assets/Scripts/GameManager/GridCreator.cs
+++ Assets/_Assets/Scripts/GameManager/GridCreator.cs
@@ -7,11 +7,52 @@ public class GridCreator : MonoBehaviour
     Vector3 gridSpawnPoint = new Vector3(-10, 0, -10);
     public int gridSizeX = 20;
     public int gridSizeZ = 20;
-    Node outOfBounds = new Node(0, 0, new Vector3(0, 10, 0), false);
+    Node outOfBounds = new Node(0, 0, new Vector3(999, 999, 999), false);
     public Node[,] grid;
     public List<Node> path;
 
-    public static List<Vector2> obstaclesCoordinates = new List<Vector2>();
+    public static List<Vector2> obstaclesCoordinates = new List<Vector2>(){
+new Vector2(0f,1f),
+new Vector2(0f,2f),
+new Vector2(0f,3f),
+new Vector2(0f,4f),
+new Vector2(0f,5f),
+new Vector2(0f,6f),
+new Vector2(0f,7f),
+new Vector2(0f,8f),
+new Vector2(0f,9f),
+new Vector2(0f,10f),
+new Vector2(0f,11f),
+new Vector2(0f,12f),
+new Vector2(0f,13f),
+new Vector2(0f,14f),
+new Vector2(0f,15f),
+new Vector2(0f,16f),
+new Vector2(0f,17f),
+new Vector2(0f,18f),
+new Vector2(0f,19f),
+new Vector2(0f,20f),
+new Vector2(1f,1f),
+new Vector2(2f,1f),
+new Vector2(3f,1f),
+new Vector2(4f,1f),
+new Vector2(5f,1f),
+new Vector2(6f,1f),
+new Vector2(7f,1f),
+new Vector2(8f,1f),
+new Vector2(9f,1f),
+new Vector2(10f,1f),
+new Vector2(11f,1f),
+new Vector2(12f,1f),
+new Vector2(13f,1f),
+new Vector2(14f,1f),
+new Vector2(15f,1f),
+new Vector2(16f,1f),
+new Vector2(17f,1f),
+new Vector2(18f,1f),
+new Vector2(19f,1f),
+};
+
     void Awake()
     {
         CreateGrid();
@@ -20,7 +61,6 @@ public class GridCreator : MonoBehaviour
 
     void CreateGrid()
     {
-        print("obstaclesCoordinates" + obstaclesCoordinates.Count);
         grid = new Node[gridSizeX, gridSizeZ];
         for (int x = 0; x < gridSizeX; x++)
         {
@@ -29,7 +69,6 @@ public class GridCreator : MonoBehaviour
                 bool walkable = true;
                 foreach (Vector2 obstacleCoord in obstaclesCoordinates)
                 {
-                    print("po");
                     if (obstacleCoord.x == x && obstacleCoord.y == z)
                     {
                         walkable = false;
@@ -55,7 +94,10 @@ public class GridCreator : MonoBehaviour
     {
         int x = Mathf.RoundToInt(worldPosition.x - gridSpawnPoint.x);
         int z = Mathf.RoundToInt(worldPosition.z - gridSpawnPoint.z);
-        if (x < 0 || z < 0 || x > grid.GetLength(0) - 1 || z > grid.GetLength(1) - 1) return outOfBounds;
+        if (x < 0 || z < 0 || x > grid.GetLength(0) - 1 || z > grid.GetLength(1) - 1)
+        {
+            return null;
+        };
         return grid[x, z];
     }
 
--- Assets/_Assets/Scripts/GameManager/Pathfinder.cs
+++ Assets/_Assets/Scripts/GameManager/Pathfinder.cs
@@ -20,43 +20,48 @@ public class Pathfinder : MonoBehaviour
 
     bool availableMovementsGridShown = false;
     bool playerCanMoveToSelectedSpot = false;
-    Vector3 lastCalculatedMovePath = new Vector3(999,999,999);
-    
+    Vector3 lastCalculatedMovePath = new Vector3(999, 999, 999);
 
-    void Awake(){
+
+    void Awake()
+    {
         grid = GetComponent<GridCreator>();
         playerStatus = transform.parent.GetChild(1).GetComponent<PlayerStatus>();
-
     }
 
 
-    List<Node> FindPath(Vector3 startPos,Vector3 endPos){
+    List<Node> FindPath(Vector3 startPos, Vector3 endPos)
+    {
         // print("find path method");
-    
+
         // Nodes that we want to calculate the F cost of
         List<Node> openNodes = new List<Node>();
         // Nodes that have already been evaluated
         List<Node> closedNodes = new List<Node>();
-    
-        
+
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(endPos);
         openNodes.Add(startNode);
-        while(openNodes.Count > 0){
+        while (openNodes.Count > 0)
+        {
             Node lowestCostNode = openNodes[0];
-            foreach(Node node in openNodes){
-                
-            if(node.fCost <= lowestCostNode.fCost && node.hCost < lowestCostNode.hCost) {
+            foreach (Node node in openNodes)
+            {
+
+                if (node.fCost <= lowestCostNode.fCost && node.hCost < lowestCostNode.hCost)
+                {
                     lowestCostNode = node;
-                    }
+                }
             }
             openNodes.Remove(lowestCostNode);
             closedNodes.Add(lowestCostNode);
 
-            if(lowestCostNode == targetNode){
+            if (lowestCostNode == targetNode)
+            {
                 List<Node> path = new List<Node>();
                 Node currentNode = targetNode;
-                while (currentNode != startNode){
+                while (currentNode != startNode)
+                {
                     path.Add(currentNode);
                     currentNode = currentNode.parent;
                 }
@@ -65,17 +70,21 @@ public class Pathfinder : MonoBehaviour
             }
 
             List<Node> neighbours = grid.GetNeighbours(lowestCostNode);
-            foreach(Node neighbour in neighbours){
-                if(!neighbour.walkable || closedNodes.Contains(neighbour)){
+            foreach (Node neighbour in neighbours)
+            {
+                if (!neighbour.walkable || closedNodes.Contains(neighbour))
+                {
                     continue;
                 }
                 int newCostToNeighbour = lowestCostNode.gCost + GetDistance(lowestCostNode, neighbour);
                 bool openNodesListContainsNeighbour = openNodes.Contains(neighbour);
-                if(newCostToNeighbour < neighbour.gCost || !openNodesListContainsNeighbour){
+                if (newCostToNeighbour < neighbour.gCost || !openNodesListContainsNeighbour)
+                {
                     neighbour.gCost = newCostToNeighbour;
                     neighbour.hCost = GetDistance(neighbour, targetNode);
                     neighbour.parent = lowestCostNode;
-                    if(!openNodesListContainsNeighbour){
+                    if (!openNodesListContainsNeighbour)
+                    {
                         openNodes.Add(neighbour);
                     }
                 }
@@ -88,7 +97,7 @@ public class Pathfinder : MonoBehaviour
 
 
     // void RetracePath(Node startNode, Node endNode){
-     
+
 
 
 
@@ -100,35 +109,40 @@ public class Pathfinder : MonoBehaviour
     //         GameObject p = Instantiate(pathIndicator, node.worldPosition, Quaternion.identity);
     //         pathView.Add(p);
     //     }
-        
+
     // }
 
-    IEnumerator MoveAgent(List<Node> path){
-        while(true){
+    IEnumerator MoveAgent(List<Node> path)
+    {
+        while (true)
+        {
             yield return new WaitForSeconds(0.01f);
 
-        // print("in move agent "+path[0].worldPosition + " 1:"+(path[0].worldPosition.x != playerStatus.player.transform.position.x)+" 2:" +(path[0].worldPosition.z != playerStatus.player.transform.position.z));
-        // print("path[0].worldPosition.x "+ path[0].worldPosition.x+ " != " + "playerStatus.player.transform.position.x "+ playerStatus.player.transform.position.x+ "&& "+ "path[0].worldPosition.z "+path[0].worldPosition.z+ "!= "+ "playerStatus.player.transform.position.z "+playerStatus.player.transform.position.z);
-        if(path.Count != 0 && path[0].worldPosition.x != playerStatus.player.transform.position.x && path[0].worldPosition.z != playerStatus.player.transform.position.z){
-            // print("movin");
-            playerStatus.player.transform.position = path[0].worldPosition;
+            // print("in move agent "+path[0].worldPosition + " 1:"+(path[0].worldPosition.x != playerStatus.player.transform.position.x)+" 2:" +(path[0].worldPosition.z != playerStatus.player.transform.position.z));
+            // print("path[0].worldPosition.x "+ path[0].worldPosition.x+ " != " + "playerStatus.player.transform.position.x "+ playerStatus.player.transform.position.x+ "&& "+ "path[0].worldPosition.z "+path[0].worldPosition.z+ "!= "+ "playerStatus.player.transform.position.z "+playerStatus.player.transform.position.z);
+            if (path.Count != 0 && path[0].worldPosition.x != playerStatus.player.transform.position.x && path[0].worldPosition.z != playerStatus.player.transform.position.z)
+            {
+                // print("movin");
+                playerStatus.player.transform.position = path[0].worldPosition;
 
-        } else if(path.Count != 0)path.RemoveAt(0);
-       
+            }
+            else if (path.Count != 0) path.RemoveAt(0);
+
         }
     }
-        
+
     //     // grid.path = path;
     // }
 
-	int GetDistance(Node nodeA, Node nodeB) {
-		int dstX = Mathf.Abs(nodeA.x - nodeB.x);
-		int dstZ = Mathf.Abs(nodeA.z - nodeB.z);
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.x - nodeB.x);
+        int dstZ = Mathf.Abs(nodeA.z - nodeB.z);
 
-		if (dstX > dstZ)
-			return 14*dstZ + 10* (dstX-dstZ);
-		return 14*dstX + 10 * (dstZ-dstX);
-	}
+        if (dstX > dstZ)
+            return 14 * dstZ + 10 * (dstX - dstZ);
+        return 14 * dstX + 10 * (dstZ - dstX);
+    }
 
 
 
@@ -136,12 +150,14 @@ public class Pathfinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!availableMovementsGridShown){
+        if (!availableMovementsGridShown)
+        {
             DateTime before = DateTime.Now;
 
 
             grid.ResetAllNodeCosts();
-            foreach(GameObject gridObj in gridView){
+            foreach (GameObject gridObj in gridView)
+            {
                 Destroy(gridObj);
             }
             gridView = new List<GameObject>();
@@ -151,26 +167,32 @@ public class Pathfinder : MonoBehaviour
             int playerPosZ = Mathf.RoundToInt(playerStatus.playerNode.worldPosition.z);
             // print("x: "+(playerPosX-maxMove)+" to "+(playerPosX+maxMove));
             // print("z: "+(playerPosZ-maxMove)+" to "+(playerPosZ+maxMove));
-            for(int x= playerPosX-maxMove;x<= playerPosX+maxMove; x++){
-                for(int z = playerPosZ-maxMove; z<=playerPosZ+maxMove;z++){
-                    if(x == playerPosX && z == playerPosZ) continue;
-                    Node currentNode = grid.NodeFromWorldPoint(new Vector3(x,0,z));
+            for (int x = playerPosX - maxMove; x <= playerPosX + maxMove; x++)
+            {
+                for (int z = playerPosZ - maxMove; z <= playerPosZ + maxMove; z++)
+                {
+                    if (x == playerPosX && z == playerPosZ) continue;
+                    Node currentNode = grid.NodeFromWorldPoint(new Vector3(x, 0, z));
                     // print(currentNode.worldPosition);
                     // print("findpath: "+playerStatus.playerNode.worldPosition+" "+(new Vector3(x,0,z)));
-                    List<Node> path = FindPath(playerStatus.playerNode.worldPosition, currentNode.worldPosition);
+
+                    List<Node> path = null;
+                    if (currentNode != null) path = FindPath(playerStatus.playerNode.worldPosition, currentNode.worldPosition);
                     // if(path.Count > 0)print("path first index:"+path[0].worldPosition);
 
 
-                    if(path.Count > 0 && path[path.Count-1].gCost <= maxMove* 10) {
+                    if (path != null && path.Count > 0 && path[path.Count - 1].gCost <= maxMove * 10)
+                    {
                         // print("instantiate");
                         // print("instantiate gameobject");
-                        GameObject p = Instantiate(moveGridIndicator, new Vector3(x,0,z), Quaternion.identity);
+                        GameObject p = Instantiate(moveGridIndicator, new Vector3(x, 0, z), Quaternion.identity);
                         gridView.Add(p);
                         // 
                     }
+
                 }
             }
-            DateTime after = DateTime.Now; 
+            DateTime after = DateTime.Now;
             TimeSpan duration = after.Subtract(before);
             Debug.Log("Duration in milliseconds: " + duration.Milliseconds);
             availableMovementsGridShown = true;
@@ -180,46 +202,56 @@ public class Pathfinder : MonoBehaviour
 
         RaycastHit hit;
         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit)) {
-        Vector3 mouseSelectWorldPosition = new Vector3(Mathf.Round(hit.point.x),mapSelector.transform.position.y,Mathf.Round(hit.point.z));
-        Transform objectHit = hit.transform;
-        mapSelector.transform.position = mouseSelectWorldPosition;
-        // print("lastCalculatedMovePath != mouseSelectWorldPosition "+lastCalculatedMovePath +" "+mouseSelectWorldPosition);
-        if(lastCalculatedMovePath != mouseSelectWorldPosition){
-            // print("calculating");
-            playerCanMoveToSelectedSpot = false;
-            foreach (GameObject indicator in pathView){
-                Destroy(indicator);
-            }
-            pathView = new List<GameObject>();
-            List<Node> path = FindPath(playerStatus.playerNode.worldPosition, mouseSelectWorldPosition);
-            // print(path);
-            
-            foreach(GameObject gridSquare in gridView ){
-                if(path.Count != 0 && gridSquare.transform.position == path[path.Count-1].worldPosition){
-                    playerCanMoveToSelectedSpot = true;
-                    // print("node found in grid");
-                    // print("instantiating path objs "+path.Count);
-                    foreach(Node node in path){
-                        GameObject p = Instantiate(movePathIndicator, node.worldPosition, Quaternion.identity);
-                        pathView.Add(p);
-                    }
-                }  
-            }
-                 lastCalculatedMovePath = mouseSelectWorldPosition;
-                return;
-        }
-        if (Input.GetMouseButtonDown(0) && playerCanMoveToSelectedSpot) {
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 mouseSelectWorldPosition = new Vector3(Mathf.Round(hit.point.x), mapSelector.transform.position.y, Mathf.Round(hit.point.z));
+            Transform objectHit = hit.transform;
+            mapSelector.transform.position = mouseSelectWorldPosition;
+            // print("lastCalculatedMovePath != mouseSelectWorldPosition "+lastCalculatedMovePath +" "+mouseSelectWorldPosition);
+            if (lastCalculatedMovePath != mouseSelectWorldPosition)
+            {
+                // print("calculating");
+                playerCanMoveToSelectedSpot = false;
+                foreach (GameObject indicator in pathView)
+                {
+                    Destroy(indicator);
+                }
+                pathView = new List<GameObject>();
+                // print(playerStatus.playerNode.worldPosition);
+                // print(mouseSelectWorldPosition);
+                List<Node> path = FindPath(playerStatus.playerNode.worldPosition, mouseSelectWorldPosition);
+                // print(path);
 
-            playerStatus.player.transform.position = mouseSelectWorldPosition;
-            playerStatus.playerNode.walkable = true;
-            playerStatus.playerNode = grid.NodeFromWorldPoint(mouseSelectWorldPosition);
-            playerStatus.playerNode.walkable = false;
-            availableMovementsGridShown = false;
-            }   
-            
-        } else mapSelector.transform.position = new Vector3(999,mapSelector.transform.position.y,999);
-        
+                foreach (GameObject gridSquare in gridView)
+                {
+                    if (path.Count != 0 && gridSquare.transform.position == path[path.Count - 1].worldPosition)
+                    {
+                        playerCanMoveToSelectedSpot = true;
+                        // print("node found in grid");
+                        // print("instantiating path objs "+path.Count);
+                        foreach (Node node in path)
+                        {
+                            GameObject p = Instantiate(movePathIndicator, node.worldPosition, Quaternion.identity);
+                            pathView.Add(p);
+                        }
+                    }
+                }
+                lastCalculatedMovePath = mouseSelectWorldPosition;
+                return;
+            }
+            if (Input.GetMouseButtonDown(0) && playerCanMoveToSelectedSpot)
+            {
+
+                playerStatus.player.transform.position = mouseSelectWorldPosition;
+                playerStatus.playerNode.walkable = true;
+                playerStatus.playerNode = grid.NodeFromWorldPoint(mouseSelectWorldPosition);
+                playerStatus.playerNode.walkable = false;
+                availableMovementsGridShown = false;
+            }
+
+        }
+        else mapSelector.transform.position = new Vector3(999, mapSelector.transform.position.y, 999);
+
     }
 }
 
