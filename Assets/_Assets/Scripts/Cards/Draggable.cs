0416e1ab0a64b16282c0c9ab55180b1087658aff
richardbonneau <richardbonneau91@gmail.com>
2020-04-11 17:32:48 -0400

    you can place cards in the deck area

--- Assets/_Assets/Scripts/Cards/Draggable.cs
+++ Assets/_Assets/Scripts/Cards/Draggable.cs
@@ -6,10 +6,16 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [System.NonSerialized] public Transform parentToReturnTo;
-    public GameObject placeholder;
+    public GameObject placeholderPrefab;
+    GameObject placeholder;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        print("begin drag");
+        placeholder = Instantiate(placeholderPrefab, this.transform.position, Quaternion.identity);
+        placeholder.transform.SetParent(this.transform.parent);
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
         parentToReturnTo = this.transform.parent;
         this.transform.SetParent(this.transform.parent.parent);
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -17,14 +23,26 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+
+        int newSiblingIndex = parentToReturnTo.childCount;
+        for (int i = 0; i < parentToReturnTo.childCount; i++)
+        {
+            if (this.transform.position.x < parentToReturnTo.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex) newSiblingIndex--;
+                break;
+            }
+
+        }
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        print("OnEndDrag");
         this.transform.SetParent(parentToReturnTo);
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-
-
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        Destroy(placeholder);
     }
 
 }
