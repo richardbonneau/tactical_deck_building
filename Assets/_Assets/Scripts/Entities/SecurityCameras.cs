0a34a611cbdf833a9775972028d94063713c3ba0
richardbonneau <richardbonneau91@gmail.com>
2020-04-15 10:45:43 -0400

    lookin good

---
+++ Assets/_Assets/Scripts/Entities/SecurityCameras.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCameras : MonoBehaviour
+{
+    GameObject player;
+
+    void Awake(){
+            player = GameObject.FindWithTag("Player");
+    }
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+}
