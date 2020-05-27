ffd470f26fdf2a871f0286e15bff30a08e09fcfa
richardbonneau <richardbonneau91@gmail.com>
2020-05-27 14:24:32 -0400

    cant figure it out

--- Assets/_Assets/Scripts/GameManager/CameraMovement.cs
+++ Assets/_Assets/Scripts/GameManager/CameraMovement.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float panSpeed = 8f;
+    public float rotateSpeed = 100f;
     public float rotationSpeed = 10f;
     public float edgeSize = 30f;
     public bool sideScrollingIsActive = false;
     public bool cameraMovementEnabled = true;
 
+    bool currentlyRotating = false;
+    string rotationDirection = "left";
+    float newYValue = 0f;
+    public float rotationAmount = 45;
+
     void Update()
     {
-
         Vector3 pos = this.transform.position;
-        Quaternion rot = this.transform.rotation;
+        float rot = this.transform.localRotation.eulerAngles.y;
         if (
             cameraMovementEnabled &&
             Input.GetKey("w")
@@ -51,7 +56,54 @@ public class CameraMovement : MonoBehaviour
         {
             pos.x += panSpeed * Time.deltaTime;
         }
+
+        if (Input.GetKeyDown("e") && !currentlyRotating)
+        {
+            currentlyRotating = true;
+            rotationDirection = "left";
+            newYValue = rot + rotationAmount;
+            if (newYValue >= 360) newYValue = 0;
+        }
+        if (Input.GetKeyDown("q") && !currentlyRotating)
+        {
+            currentlyRotating = true;
+            rotationDirection = "right";
+            newYValue = rot - rotationAmount;
+            if (newYValue >= 360) newYValue = 0;
+        }
         this.transform.position = pos;
+
+
+        if (currentlyRotating && rotationDirection == "left")
+        {
+            if (rot < newYValue || newYValue == 0 && rot > 0 && rot > 5)
+            {
+
+                this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+            }
+            else if (rot > newYValue)
+            {
+                Vector3 eulerAngles = this.transform.eulerAngles;
+                eulerAngles.y = newYValue;
+                this.transform.eulerAngles = eulerAngles;
+                currentlyRotating = false;
+            }
+        }
+        else if (currentlyRotating && rotationDirection == "right")
+        {
+            if (rot > newYValue || newYValue == 0 && rot > 0 && rot < 355)
+            {
+
+                this.transform.Rotate(Vector3.down * Time.deltaTime * rotateSpeed);
+            }
+            else if (rot < newYValue || (newYValue == 0 && rot > 355))
+            {
+                Vector3 eulerAngles = this.transform.eulerAngles;
+                eulerAngles.y = newYValue;
+                this.transform.eulerAngles = eulerAngles;
+                currentlyRotating = false;
+            }
+        }
 
     }
 }
