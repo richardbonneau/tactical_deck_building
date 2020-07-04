9d9282ef722dcab37a7da238430d4b7d6bc1faf9
richardbonneau <richardbonneau91@gmail.com>
2020-07-04 13:14:59 -0400

    random obstacles

---
+++ Assets/_Assets/Scripts/LevelCreator/ObstacleCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCreator : MonoBehaviour
+{
+    public Transform obstaclePositionsParent;
+    
+
+    public GameObject[] randomObstacles;
+
+    void Start()
+    {
+
+        foreach(Transform obstacle in obstaclePositionsParent){
+            Instantiate(randomObstacles[Random.Range(0,randomObstacles.Length)], new Vector3(obstacle.position.x,0,obstacle.position.z), Quaternion.identity);
+        }
+        
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
