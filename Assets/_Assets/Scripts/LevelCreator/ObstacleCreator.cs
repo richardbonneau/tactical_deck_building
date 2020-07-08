82dd5a0d96b52ccd9eee303625de67a9fbac47de
richardbonneau <richardbonneau91@gmail.com>
2020-07-08 16:41:45 -0400

    basic creation and deletion of obstacles

--- Assets/_Assets/Scripts/LevelCreator/ObstacleCreator.cs
+++ Assets/_Assets/Scripts/LevelCreator/ObstacleCreator.cs
@@ -6,22 +6,27 @@ public class ObstacleCreator : MonoBehaviour
 {
     public Transform obstaclePositionsParent;
     
-
+    // public GameObject[] bases;
     public GameObject[] randomObstacles;
+    List<GameObject> createdObstacles = new List<GameObject>();
 
     void Start()
     {
-
-        foreach(Transform obstacle in obstaclePositionsParent){
-            Instantiate(randomObstacles[Random.Range(0,randomObstacles.Length)], new Vector3(obstacle.position.x,0,obstacle.position.z), Quaternion.identity);
-        }
-        
-        
+   
     }
 
+    public void createLevel(){
+         foreach(Transform obstacle in obstaclePositionsParent){
+            GameObject newObstacle = Instantiate(randomObstacles[Random.Range(0,randomObstacles.Length)], new Vector3(obstacle.position.x,0,obstacle.position.z), Quaternion.identity);
+            createdObstacles.Add(newObstacle);
+            
+        }
+    }
 
-    void Update()
-    {
+    public void destroyLevel(){
+         foreach(GameObject obstacle in createdObstacles){
+            Destroy(obstacle);
+        }
         
     }
 }
